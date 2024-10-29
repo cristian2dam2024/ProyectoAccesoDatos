6005427f2ffbb018de14b5de0efcc9728b834d2f
cristian2dam2024 <cristian2dam2024@gmail.com>
2024-10-29 18:27:09 +0100

    problema librerias solucionado

--- AccesoDatos/src/main/java/org/corella/AccesoDatos/Main.java
+++ AccesoDatos/src/main/java/org/corella/AccesoDatos/Main.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        PruebaDirectorio();
+        //PruebaDirectorio();
         //PruebaReaderWriter();
         //PruebaCSV();
         //PruebaIOStream();
