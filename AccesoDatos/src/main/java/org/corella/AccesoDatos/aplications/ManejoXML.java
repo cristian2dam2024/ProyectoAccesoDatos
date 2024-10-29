d25225c7dea7e43b8caf93646e885ba3607f4fe4
cristian2dam2024 <cristian2dam2024@gmail.com>
2024-10-29 18:17:40 +0100

    Clase 29-10-2024 XML

--- AccesoDatos/src/main/java/org/corella/AccesoDatos/aplications/ManejoXML.java
+++ AccesoDatos/src/main/java/org/corella/AccesoDatos/aplications/ManejoXML.java
@@ -3,11 +3,22 @@ import org.corella.AccesoDatos.utilsAcceso.Constantes;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.io.StringWriter;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -19,9 +30,71 @@ public class ManejoXML {
 
     public void run(){
 
-        Document documentoXML = leerXML(Constantes.pruebaxml);
+        //Document documentoXML = leerXML(Constantes.pruebaxml);
+    	try {
+			escribirXML();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
     }
+    
+    private void escribirXML() throws ParserConfigurationException{
+    	try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document documento = db.newDocument();		
+			
+			documento.setXmlVersion("1.0");
+			
+			Element elementoClientes = documento.createElement("clientes");
+			documento.appendChild(elementoClientes); // está en la raiz del documento
+			
+			Element elementoCliente = documento.createElement("cliente");
+			elementoClientes.appendChild(elementoCliente);
+			
+			elementoCliente.setAttribute("DNI", "123456789M");
+			// ... todos los atributos que queramos 
+			
+			Element nombre = documento.createElement("NOMBRE");
+			elementoCliente.appendChild(nombre);
+			nombre.appendChild(documento.createTextNode("Darío"));
+			
+			// -------------------------------------------------------
+			
+			DOMSource domSource = new DOMSource(documento);
+			
+			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+			transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+			
+		
+			StringWriter sw = new StringWriter();
+			StreamResult sr = new StreamResult(sw);
+			transformer.transform(domSource, sr);
+			
+			System.out.println(sw.toString());
+			
+			
+			
+		} catch (ParserConfigurationException parserException) {
+			System.err.println(parserException.getMessage());
+			
+		} catch (TransformerConfigurationException e) {
+			
+			e.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e) {
+			
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
 
     private static void muestraNodo(Node nodo, PrintStream printStream, int nivel) {
 
