473ee6e0f3e32da3ad1d7b7cbf5bcbdb69ee6ba5
cristian2dam2024 <cristian2dam2024@gmail.com>
2024-11-20 18:51:31 +0100

    Ejercicio query - incompleto

--- AccesoDatos/src/main/java/jdbc/ConectorMYSQL.java
+++ AccesoDatos/src/main/java/jdbc/ConectorMYSQL.java
@@ -1,19 +1,148 @@
 package jdbc;
 import java.sql.*;
+import java.util.Scanner;
 
 public class ConectorMYSQL {
 	
+
 	public void run() {
 		
 		try {
 			Connection conexion = creaConexion();
 			//selectAlumnos(conexion);
-			selectAlumnosPrepared(conexion);
-			
+			//selectAlumnosPrepared(conexion);
+			salarioMedioDepartamentos(conexion);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	//CRUD: crear, leer, update, borrar sobre la tabla departaments
+	
+	public void operacionCRUD(Connection conexion) throws SQLException {
+		
+		Statement sentencia = conexion.createStatement();
+		Scanner scanner = new Scanner(System.in);
+		ResultSet salidaQuery;
+		
+		System.out.println("Que tipo de operacion va a realizar? (crear - leer - modificar - borrar)");
+		String operacion = scanner.nextLine();
+		
+		System.out.println("\nTodas las operaciones se realizan sobre la tabla 'departments' de la base de datos");
+		String entrada;
+		
+	
+		switch (operacion) {
+		case "crear":
+
+			String dept_no;
+			String dept_name;
+			
+			salidaQuery = sentencia.executeQuery("SELECT COUNT (*) FROM departments");
+			int numeroRegistros = Integer.parseInt(salidaQuery.toString());
+			dept_no = "d00"+String.valueOf(numeroRegistros+1);
+			
+			System.out.println("Que nombre quiere que tenga el nuevo departamento?");
+			dept_name = scanner.nextLine();
+			
+			sentencia.executeUpdate("INSERT INTO department VALUES ("+dept_no+","+dept_name+");");
+			
+			
+			break;
+		case "leer":
+			
+			System.out.println("Introduzca el numero del departamento (d00+numero) o el nombre del departamento que quiere consultar.");
+			entrada = scanner.nextLine();
+			
+			salidaQuery = sentencia.executeQuery("SELECT FROM departments WHERE dept_no = "+ entrada + " OR dept_name = " + entrada);
+			imprimeSalida(salidaQuery);
+
+			break;
+		case "update":
+			try {
+				
+				PreparedStatement sentenciaPreparada = conexion.prepareStatement("UPDATE departments SET ? = ? WHERE ? = ? ");
+				
+				System.out.println("Introduzca el numero del departamento (d00+numero) o el nombre del departamento que quiere modificar.");
+				entrada = scanner.nextLine();
+				 
+				
+				
+			} catch (SQLIntegrityConstraintViolationException e) {
+				// TODO: handle exception
+			}
+			
+			
+			
+			
+
+			break;
+		case "borrar":
+
+			break;
+
+		default:
+			break;
+		}
+		
+		conexion.commit();
+		
+		scanner.close();
+		sentencia.close();
+		salidaQuery.close();
+		conexion.close();
+	}
+	
+	
+	
+	
+	
+	
+	//Consultar salario medio de los empleados de cada departamento
+		// salario total del departamento/numero de empleados del departamento
+		//  cada empleado tiene un salario y esta asociado a un departamento
+		// 
+		// empleado (tabla empleados) -> check dept numero
+	private void salarioMedioDepartamentos(Connection conexion) throws SQLException{
+		
+		Statement sentencia = conexion.createStatement();
+		
+		String query = "SELECT\r\n"
+				+ "    e.dept_no,\r\n"
+				+ "    COUNT(*) AS numero_empleados,\r\n"
+				+ "    SUM(s.salary) AS suma_salarios,\r\n"
+				+ "    AVG(s.salary) AS media_salarios\r\n"
+				+ "FROM (\r\n"
+				+ "    -- Empleados que son managers\r\n"
+				+ "    SELECT managers.emp_no, managers.dept_no\r\n"
+				+ "    FROM dept_manager managers\r\n"
+				+ "\r\n"
+				+ "    UNION ALL\r\n"
+				+ "\r\n"
+				+ "    -- Empleados normales\r\n"
+				+ "    SELECT otros.emp_no, otros.dept_no\r\n"
+				+ "    FROM dept_emp otros\r\n"
+				+ ") AS e\r\n"
+				+ "JOIN salaries s ON e.emp_no = s.emp_no\r\n"
+				+ "GROUP BY e.dept_no;";
+		
+		ResultSet salidaQuery = sentencia.executeQuery(query);
+		int numeroColumnas = salidaQuery.getMetaData().getColumnCount();
+		while (salidaQuery.next()) {
+
+			for (int i = 1; i <= numeroColumnas; i++) {
+				System.out.print(salidaQuery.getObject(i) + " ");
+			}
+			System.out.println();
+		}
+
+		sentencia.close();
+		salidaQuery.close();
+		conexion.close();
+		
+		
+		
 	}
 	
 
@@ -30,14 +159,9 @@ public class ConectorMYSQL {
 			String id = "2";
 			pr.setString(1, id); //el set debe corresponderse con el tipo de dato que tenemos en la base de datos
 			ResultSet salidaQuery = pr.executeQuery();
-			int numeroColumnas = salidaQuery.getMetaData().getColumnCount();
-			while(salidaQuery.next()) {
-				
-				for (int i = 1; i <= numeroColumnas; i++) {
-					System.out.print(salidaQuery.getObject(i) +" ");
-				}
-				System.out.println();
-			}
+			
+			imprimeSalida(salidaQuery);
+			
 			pr.close();
 			
 			String query3 = "INSERT INTO alumnos VALUES (?,?,?,?)";
@@ -70,6 +194,18 @@ public class ConectorMYSQL {
 		return null;
 	} 
 	
+
+	private void imprimeSalida(ResultSet salidaQuery) throws SQLException {
+		// TODO Auto-generated method stub
+		int numeroColumnas = salidaQuery.getMetaData().getColumnCount();
+		while(salidaQuery.next()) {
+			
+			for (int i = 1; i <= numeroColumnas; i++) {
+				System.out.print(salidaQuery.getObject(i) +" ");
+			}
+			System.out.println();
+		}
+	}
 
 	private void selectAlumnos(Connection conexion) throws SQLException {
 		Statement sentencia = conexion.createStatement();
